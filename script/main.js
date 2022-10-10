0af854d8bbff8a220246bd2e8c6407c3336879dd
hkimi amin <hkimiamin02@gmail.com>
2022-10-10 16:50:00 +0100

    reciept

--- script/main.js
+++ script/main.js
@@ -2,12 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             articles: [
-                { id: 1, name: "Antilla", price: 10000000000, path: "assets/images/antilla.jfif", quantity: 0 },
-                { id: 2, name: "Antilla", price: 10000000000, path: "./assets/images/antilla.jfif", quantity: 0 },
-                { id: 3, name: "Antilla", price: 10000000000, path: "./assets/images/antilla.jfif", quantity: 0 },
+                { id: 1, name: "Antilla", price: 10000000000, path: "./assets/images/antilla.jfif", quantity: 0 },
+                { id: 2, name: "iphone 14", price: 1099, path: "./assets/images/iphone14.jfif", quantity: 0 },
+                { id: 3, name: "play station 5", price: 499.99, path: "./assets/images/ps5.jpg", quantity: 0 },
             ],
             cart: 0,
             fortune: 129000000000,
+            purchases: [],
         }
     },
     methods: {
@@ -15,6 +16,15 @@ const app = Vue.createApp({
             if (this.fortune >= article.price) {
                 this.fortune -= article.price;
                 this.cart++;
+                if (article.quantity == 0) {
+                    this.purchases.push(article);
+                } else {
+                    let item = this.purchases.find(x => x.id == article.id);
+                    if (item) {
+                        item.quantity = article.quantity;
+                    }
+
+                }
                 article.quantity++;
             }
         },
@@ -23,8 +33,23 @@ const app = Vue.createApp({
                 this.fortune += article.price;
                 this.cart--;
                 article.quantity--;
+                if (article.quantity == 0) {
+                    let item = this.purchases.find(x => x.id == article.id);
+                    this.purchases.splice(this.purchases.indexOf(item), 1);
+                }
             }
         }
+    },
+    computed: {
+        /*checkPrice(price) {
+            console.log(data.fortune);
+            return data.fortune < price;
+        },
+        checkQuantity(quantity) {
+            console.log(quantity);
+            return quantity == 0;
+        }*/
+
     },
 });
 const mountedApp = app.mount("#app");
